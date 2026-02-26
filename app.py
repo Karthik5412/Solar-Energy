13684d7b84babefc4f2f8bb80ac1872a7bf612da
Karthik5412 <kkuricharlapati@gmail.com>
2026-02-26 21:05:40 +0530

    op coming

--- app.py
+++ app.py
@@ -2,21 +2,19 @@ from transformers import Preprocessing, Encoding
 import pandas as pd
 import numpy as np
 import streamlit as st
-from datetime import timedelta, combine, datetime
+from datetime import timedelta,  datetime
 import joblib
 import requests
 
 date = st.sidebar.date_input('Enter date :')
-time = st.sidebar.time_input('Enter time :')
+time = st.sidebar.time_input('Enter time :').strftime('%H')
 
-st_date = combine(date,time) 
-en_date = st_date + timedelta(hours= 1 )
+st_date = date
+en_date = st_date + timedelta(days= 1 )
 
-st_date_str = datetime(st_date).strftime('%Y-%m-%dT%H:%M')
-en_date_str = datetime(en_date).strftime('%Y-%m-%dT%H:%M')
 
 base_url = 'https://api.open-meteo.com/v1/forecast?latitude=17.3850&longitude=78.4867&hourly=temperature_2m,direct_radiation,diffuse_radiation'
-date_range = f'&start_hour={st_date_str}&end_hour={en_date_str}'
+date_range = f'&start_date={st_date}&end_date={en_date}'
 time_zone = r'&timezone=Asia%2FKolkata'
 
 url = base_url+date_range + time_zone
@@ -26,17 +24,21 @@ response = requests.get(url).json()
 NOCT = 45.0
 current = response['hourly']
 
-temp = current['temperature_2m']
-irr = current['direct_radiation'] + current['diffuse_radiation']
+temp = current['temperature_2m'][int(time)]
+
+irr = current['direct_radiation'][int(time)] + current['diffuse_radiation'][int(time)]
 
 module = temp + ((NOCT - 20) / 800) * irr
+
 
 pipeline = joblib.load(r'pipe.plk')
 
 
+dt_format = pd.to_datetime(current['time'][int(time)]).strftime('%Y-%m-%d %H:%M:%S')
+
 
 data = {
-    'DATE_TIME' : [current['time']] ,
+    'DATE_TIME' : [dt_format] ,
     'AMBIENT_TEMPERATURE' : [temp] ,
     'MODULE_TEMPERATURE' : [module],
     'IRRADIATION' : [irr]
@@ -44,5 +46,7 @@ data = {
 
 df = pd.DataFrame(data)
 
-st.success(df)
-st.success(pipeline.predict(df))
+pred = pipeline.predict(df)[0]
+pred = list(pred)
+st.success(pred[0 :])
+# st.success(f'DC_POWER : {pred[0]} DAY_DC_POWER : {pred[1]}')
