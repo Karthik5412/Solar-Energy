2699c533642c0d599b05b5681719b6de68c49ae5
Karthik5412 <kkuricharlapati@gmail.com>
2026-02-27 20:15:06 +0530

    Charts

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from datetime import timedelta,  datetime
 import joblib
 import requests
+import plotly.graph_objects as go 
 
 st.set_page_config(page_title='Solar', page_icon='🌞', layout='wide')
 
@@ -52,6 +53,34 @@ try :
 
     pred = pipeline.predict(df)[0]
     pred = list(pred)
+    
+    dia1,dia2 = st.columns(2)
+    
+    with dia1:
+       fig = go.Figure(
+           go.Indicator(
+               mode='gauge+number',
+               value= temp,
+               domain= {'x' : [0,1], 'y' : [0,1]},
+               gauge={
+                'axis' : {'range' : [None, 100], 'thickwidth' :1, 'thickcolor' : 'brown'},
+                'bar' : 'grey',
+                'bgcolor' : 'green',
+                'bodercolor' : 'black',
+                'steps' : [
+                    { 'range': [0, 15], 'color': 'blue'},
+                    {'range': [15, 30], 'color': 'green'},
+                    {'range': [30, 100], 'color': 'red'}
+                ],
+                'threshold' :{
+                    'line': {'color': "black", 'width': 4},
+                    'thickness': 0.75,
+                    'value': 90
+                    }
+               }
+           )
+        )
+       st.plotly_chart(fig, use_container_width=True)
 
     col1, col2, col3 = st.columns(3)
 
@@ -64,6 +93,8 @@ try :
     with col3 :
         st.subheader('Module Temperature ')
         st.subheader(f'{module} °C' )
+        
+
         
 
     # st.success()
