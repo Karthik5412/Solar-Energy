e707efe0316977f410fecdcd0c9868066f64d75e
Karthik5412 <kkuricharlapati@gmail.com>
2026-03-01 15:35:54 +0530

    Almost done

--- app.py
+++ app.py
@@ -9,7 +9,24 @@ import plotly.graph_objects as go
 
 st.set_page_config(page_title='Solar', page_icon='🌞', layout='wide')
 
-st.title('Solar Production Predictor')
+st.markdown( '''<style>
+            .stApp {
+                background : linear-gradient(135deg, #fceABB 0%, #f8b500 100%);
+                color : #CD5B45;
+            }
+            
+            stPlotlyChart{
+                background-color : tranparent ! important;
+                border-radius : 15px ;
+                box-shadow : 0 4px 6px rgba(0,0,0,0.1);
+                padding : 10px;
+            }
+            
+            </style>
+            ''',unsafe_allow_html=True
+)
+
+st.title('Solar Production Predictor', text_alignment= 'center')
 
 date = st.sidebar.date_input('Enter date :')
 time = st.sidebar.time_input('Enter time :').strftime('%H')
@@ -54,34 +71,6 @@ df = pd.DataFrame(data)
 pred = pipeline.predict(df)[0]
 pred = list(pred)
 
-dia1,dia2 = st.columns(2)
-
-with dia1:
-    fig = go.Figure(
-        go.Indicator(
-            mode='gauge+number',
-            value= temp,
-            domain= {'x' : [0,1], 'y' : [0,1]},
-            gauge={
-            'axis' : {'range' : [0, 55], 'tickwidth' :2, 'tickcolor' : 'brown'},
-            'bar' : {'color' : 'grey'},
-            'bgcolor' : 'green',
-            'bordercolor' : 'black',
-            'steps' : [
-                { 'range': [0, 15], 'color': 'blue'},
-                {'range': [15, 30], 'color': 'green'},
-                {'range': [30, 55], 'color': 'red'}
-            ],
-            # 'threshold' :{
-            #     'line': {'color': "black", 'width': 4},
-            #     'thickness': 0.75,
-            #     'value': 90
-            #     }
-            }
-        )
-    )
-    st.plotly_chart(fig, use_container_width=True)
-
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -94,6 +83,56 @@ with col3 :
     st.subheader('Module Temperature ')
     st.subheader(f'{module} °C' )
     
+dia1,dia2 = st.columns(2)
+
+with dia1:
+    
+    if temp < 15 :
+        bar_color = 'skyblue'
+        text_color = '#1A237E'
+        border_color = 'blue'
+    elif temp > 15 and temp < 30 :
+        bar_color = 'springgreen'
+        text_color = '#2E7D32'
+        border_color = 'green'
+    else :
+        bar_color = 'orange'
+        text_color = '#B71C1C'
+        border_color = 'red'
+    
+    fig = go.Figure(
+        go.Indicator(
+            mode='gauge+number',
+            title= {
+                'text' : '<b>TEMPERATURE</b>',
+                'font' : {
+                'color' : '#CD5B45',
+                'size'  : 24
+                }
+                } ,
+            value= temp,
+            domain= {'x' : [0,1], 'y' : [0,1]},
+            number={
+                'font' : {'color' : text_color},
+                'suffix' : ' °C'
+                },
+            gauge={
+            'axis' : {'range' : [0, 55], 'tickwidth' :2, 'tickcolor' : 'red', 'tickfont' : {'color' : 'red', 'size' : 14}},
+            'bar' : {'color' : bar_color},
+            'bgcolor' : 'black',
+            'borderwidth' : 3,
+            'bordercolor' : border_color,
+            'threshold': {
+            'line': {'color': "red", 'width': 4},
+            'thickness': 0.75,
+            'value': 90
+        }
+            }
+        )
+    )
+    
+    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
+    st.plotly_chart(fig, use_container_width=True)
 
     
 
